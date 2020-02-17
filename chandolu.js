c67ceb29268307ae7fc737f33d1eabcbbf0ae29f
annie0sc <annie0sc@gmail.com>
2020-02-17 16:17:10 -0600

    Changed the formula

--- chandolu.js
+++ chandolu.js
@@ -1,15 +1,15 @@
 function calc(){
-let totalin = document.getElementById("totalin").value;
+let totalin = parseFloat(document.getElementById("totalin").value);
 console.log(typeof totalin);
-let tipin = document.getElementById("tipin").value;
+let tipin = parseFloat(document.getElementById("tipin").value);
 
 if(totalin == "" || tipin == "") {
-    alert("enter correctly -_-");
+    alert("-_- ENTER CORRECTLT *_*");
 }
 else{
-    document.getElementById("tipout").innerHTML = "$" + totalin*tipin;
-    document.getElementById("taxout").innerHTML = "$" + totalin*0.055;
-    document.getElementById("grandout").innerHTML = "$" + (totalin + (totalin*tipin) + (totalin*0.055));
+    document.getElementById("tipout").innerHTML = "$" + totalin*(tipin/100.0);
+    document.getElementById("taxout").innerHTML = 5.50 + "%";
+    document.getElementById("grandout").innerHTML = "$" + (totalin + (totalin*(tipin/100)) + (totalin*0.055));
     return false;
 }
 }
